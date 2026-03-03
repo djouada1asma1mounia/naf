a33a30856fb7ec5f944996bb680fc010c390640b
BOUAZZA karim <163033054+Karim-Bouazza@users.noreply.github.com>
2026-03-03 17:12:39 +0100

    setup database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env.dev',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('HOST')!,
+        port: parseInt(config.get<string>('PORT')!, 10),
+        username: config.get<string>('USERNAMEDB')!,
+        password: config.get<string>('PASSWORD')!,
+        database: config.get<string>('DATABASE')!,
+        synchronize: config.get<string>('SYNCHRONIZE') === 'true',
+        autoLoadEntities: true,
+      }),
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
