1d6dfb52a4f9aec0e0b27c7f9bb6d7336ea1cc87
BOUAZZA karim <163033054+Karim-Bouazza@users.noreply.github.com>
2026-03-12 00:15:34 +0100

    start department

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
